3c1a6a57f96c4860d473371c102ee16404967e7a
Can ŞAFAK <safaksafakcan@gmail.com>
2024-05-01 11:00:55 +0300

    Passport js paketinin kurulması.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './modules/auth/auth/auth.module';
+import { Module } from './modules/auth/.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, Module],
   controllers: [AppController],
   providers: [AppService],
 })
